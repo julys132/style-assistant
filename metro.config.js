e243e41f8b16fe70ddf5e499a80c82feb9cbd5c9
julys132 <iuliastarcean1@gmail.com>
2026-03-06 18:14:39 +0000

    Add Node compatibility fallback for Metro toReversed

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,13 @@
+if (!Array.prototype.toReversed) {
+  Object.defineProperty(Array.prototype, "toReversed", {
+    value: function toReversed() {
+      return [...this].reverse();
+    },
+    writable: true,
+    configurable: true,
+  });
+}
+
 const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
